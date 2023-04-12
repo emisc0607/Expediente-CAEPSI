174773dda5f2ef6cd9710fa1eac918b74134e391
Emilano Sánchez <62582536+emisc0607@users.noreply.github.com>
2023-04-12 12:03:36 -0600

    Recycler View created

---
+++ app/src/main/java/com/emisc0607/expedientecaepsi/entities/Expediente.kt
@@ -0,0 +1,6 @@
+package com.emisc0607.expedientecaepsi.entities
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Expediente(var id: String = "", var name: String = "", var age: String = "", var imgUrl:String = "")
